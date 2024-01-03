ca9f7dc0b6164d7413bf3e7b7333a8990c7689d7
jaimeserranodev <jaime.serrano.dev@gmail.com>
2024-01-03 16:29:36 +0000

    working in home

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,27 @@
 import '../styles/globals.css';
 
+// router
+import { useRouter } from 'next/router';
 
 //components
 import Layout from '../components/Layout';
-
-// router
-import { useRouter } from 'next/router';
+import  Transition  from '../components/Transition';
 
 // framer motion
 import { AnimatePresence, motion } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
   <Layout>
+    <AnimatePresence mode='wait'>
+      <motion.div key={router.route} className='h-full'>
+        <Transition />
         <Component {...pageProps} />
+      </motion.div>
+      
+    </AnimatePresence>
+        
   </Layout>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+// next image
+import Image from "next/image";
+
+// components
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+
+// framer motion
+import { motion } from 'framer-motion';
+
+// variants
+import { fadeIn } from '../variants';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+  <div className="bg-primary/60 h-full">
+    {/* text */}
+    <div>
+      <div>
+        {/* title */}
+        <h1>Transforming Ideas <br /> Into{' '}
+          <span className="text-accent">Digital Reality</span>
+        </h1>
+        {/* subtitle */}
+        <p></p>
+      </div>
+    </div>
+    {/* image */}
+    <div>image</div>
+  </div>
+  );
 };
 
 export default Home;
