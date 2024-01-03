ebbb3a567037850414fc750ce395d311b25d44c6
jaimeserranodev <jaime.serrano.dev@gmail.com>
2024-01-03 13:24:26 +0000

    Navegador completed

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,5 @@
 
-import { Sora } from '@next/font/google'
+import {Sora} from '@next/font/google'
 
 //font settings
 const sora = Sora ({
@@ -10,12 +10,13 @@ const sora = Sora ({
 
 // components
 import Nav from '../components/Nav';
-import { Header } from '../components/Header';
+import  Header  from '../components/Header';
 import TopLeftImg from '../components/TopLeftImg';
 
 const Layout = ({children}) => {
   return (
-  <div>
+  <div 
+    className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative`}>
       <TopLeftImg />
       <Nav />
       <Header />
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css';
 
+
+//components
+import Layout from '../components/Layout';
+
+// router
+import { useRouter } from 'next/router';
+
+// framer motion
+import { AnimatePresence, motion } from 'framer-motion';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <Layout>
+        <Component {...pageProps} />
+  </Layout>
+  )
 }
 
 export default MyApp;
