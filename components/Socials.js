aad146826de60151d825a2d4aeb4e26016cd97b0
jaimeserranodev <jaime.serrano.dev@gmail.com>
2024-01-06 13:02:54 +0000

    header

--- components/Socials.js
+++ components/Socials.js
@@ -1,3 +1,9 @@
+// links
+import Link from "next/link";
+
+// icons
+import { RiYoutubeLine, RiInstagramLine, RiInstagramLine, RiFacebookline, RiOribbbleLine, RiBehanceLine, RiPinterestLine,} from
+
 const Socials = () => {
   return <div>Socials</div>;
 };
